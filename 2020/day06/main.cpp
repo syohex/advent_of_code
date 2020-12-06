8bded51b52568f34a6f38db80259360dbc98835a
Shohei YOSHIDA <syohex@gmail.com>
2020-12-06 14:49:29 +0900

    Add day06 part02

--- 2020/day06/main.cpp
+++ 2020/day06/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <map>
 
 namespace {
 
@@ -25,6 +26,43 @@ int Solve01(const std::vector<std::string> &input) {
     return ret;
 }
 
+int Solve02(const std::vector<std::string> &input) {
+    int ret = 0;
+    std::map<char, int> m;
+    int persons = 0;
+    for (const auto &line : input) {
+        if (line.empty()) {
+            int count = 0;
+
+            for (const auto &it : m) {
+                if (it.second == persons) {
+                    ++count;
+                }
+            }
+
+            ret += count;
+            persons = 0;
+            m.clear();
+            continue;
+        }
+
+        ++persons;
+        for (const char c : line) {
+            ++m[c];
+        }
+    }
+
+    int count = 0;
+    for (const auto &it : m) {
+        if (it.second == persons) {
+            ++count;
+        }
+    }
+
+    ret += count;
+    return ret;
+}
+
 void Test01() {
     std::vector<std::string> input{"abc",
                                    "", //
@@ -39,10 +77,25 @@ void Test01() {
     assert(Solve01(input) == 11);
 }
 
+void Test02() {
+    std::vector<std::string> input{"abc",
+                                   "", //
+                                   "a",   "b",  "c",
+                                   "", //
+                                   "ab",  "ac",
+                                   "", //
+                                   "a",   "a",  "a", "a",
+                                   "", //
+                                   "b"};
+
+    assert(Solve02(input) == 6);
+}
+
 } // namespace
 
 int main() {
     Test01();
+    Test02();
 
     std::vector<std::string> input;
     std::string line;
@@ -51,5 +104,6 @@ int main() {
     }
 
     std::cout << "Part01: " << Solve01(input) << std::endl;
+    std::cout << "Part02: " << Solve02(input) << std::endl;
     return 0;
 }
