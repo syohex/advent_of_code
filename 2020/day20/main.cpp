c3c746ab6239037df7d69ac9326f607bc8195f4e
Shohei YOSHIDA <syohex@gmail.com>
2020-12-20 18:36:59 +0900

    Approval part01

--- 2020/day20/main.cpp
+++ 2020/day20/main.cpp
@@ -8,6 +8,7 @@
 #include <functional>
 #include <cmath>
 #include <map>
+#include <set>
 
 namespace {
 
@@ -28,8 +29,19 @@ enum class FlipType {
 struct Image {
     std::int64_t id = -1;
     std::vector<std::string> tiles;
+    std::set<std::vector<std::string>> candidates;
 
-    static std::vector<std::string> Flip1(const std::vector<std::string>& data, bool vertical) {
+    Image() = default;
+
+    explicit Image(std::int64_t id, std::vector<std::string> tiles) : id(id), tiles(std::move(tiles)) {
+        for (const auto flip_type : {FlipType::kNone, FlipType::kVertical, FlipType::kHorizontal, FlipType::kBoth}) {
+            for (const auto rotate_type : {RotateType::kNone, RotateType::k90, RotateType::k180, RotateType::k270}) {
+                candidates.insert(Transform(flip_type, rotate_type));
+            }
+        }
+    }
+
+    static std::vector<std::string> Flip1(const std::vector<std::string> &data, bool vertical) {
         std::vector<std::string> ret;
         if (vertical) {
             for (const auto &tile : data) {
@@ -51,7 +63,7 @@ struct Image {
         return Flip1(tiles, vertical);
     }
 
-    static std::vector<std::string> Rotate1(const std::vector<std::string>& data, int degree) {
+    static std::vector<std::string> Rotate1(const std::vector<std::string> &data, int degree) {
         std::vector<std::string> ret;
         int count = degree / 90;
         switch (count) {
@@ -89,7 +101,6 @@ struct Image {
         }
 
         return ret;
-
     }
 
     std::vector<std::string> Rotate(int degree) const {
@@ -129,28 +140,29 @@ struct Image {
 };
 
 template <typename T>
-std::vector<Image> ParseInput(T &input_stream) {
-    std::vector<Image> ret;
+std::map<std::int64_t, Image> ParseInput(T &input_stream) {
+    std::map<std::int64_t, Image> ret;
     std::string line;
 
-    Image image;
+    std::int64_t image_id;
+    std::vector<std::string> tiles;
     while (std::getline(input_stream, line)) {
         if (line.find("Tile ") == 0) {
             int id;
             sscanf(line.c_str(), "Tile %d:", &id);
-            image.id = id;
+            image_id = id;
         } else if (line.empty()) {
-            assert(image.id != -1 && !image.tiles.empty());
-            ret.push_back(image);
-            image.tiles.clear();
+            assert(image_id != -1 && !tiles.empty());
+            ret[image_id] = Image(image_id, tiles);
+            tiles.clear();
             continue;
         } else {
-            image.tiles.push_back(line);
+            tiles.push_back(line);
         }
     }
 
-    if (!image.tiles.empty()) {
-        ret.push_back(image);
+    if (!tiles.empty()) {
+        ret[image_id] = Image(image_id, tiles);
     }
 
     return ret;
@@ -184,20 +196,20 @@ bool CanPut(const std::vector<Info> &infos, const std::vector<std::string> &data
     return true;
 }
 
-std::int64_t Solve01(const std::vector<Image> &images) {
+std::int64_t Solve01(const std::map<std::int64_t, Image> &images) {
     size_t limit = images.size();
     size_t row_size = static_cast<size_t>(std::sqrt(limit));
 
     std::vector<Info> answer;
     std::function<bool(size_t pos, const std::vector<Info> &acc)> f;
     f = [&f, row_size, &images, &answer](size_t pos, const std::vector<Info> &acc) -> bool {
-        printf("@@ acc=%zd\n", acc.size());
         if (pos == images.size()) {
             answer = acc;
             return true;
         }
 
-        for (const auto &image : images) {
+        for (const auto &it : images) {
+            auto &image = it.second;
             bool already_used = false;
             for (const auto &info : acc) {
                 if (image.id == info.id) {
@@ -210,15 +222,12 @@ std::int64_t Solve01(const std::vector<Image> &images) {
                 continue;
             }
 
-            for (const auto flip_type : {FlipType::kNone, FlipType::kVertical, FlipType::kHorizontal, FlipType::kBoth}) {
-                for (const auto rotate_type : {RotateType::kNone, RotateType::k90, RotateType::k180, RotateType::k270}) {
-                    auto transformed_data = image.Transform(flip_type, rotate_type);
-                    if (CanPut(acc, transformed_data, row_size, pos)) {
-                        auto tmp = acc;
-                        tmp.emplace_back(Info{image.id, transformed_data});
-                        if (f(pos + 1, tmp)) {
-                            return true;
-                        }
+            for (const auto &candidate : image.candidates) {
+                if (CanPut(acc, candidate, row_size, pos)) {
+                    auto tmp = acc;
+                    tmp.emplace_back(Info{image.id, candidate});
+                    if (f(pos + 1, tmp)) {
+                        return true;
                     }
                 }
             }
@@ -348,17 +357,18 @@ Tile 3079:
     std::istringstream ss(input);
     auto images = ParseInput(ss);
     assert(images.size() == 9);
-    assert(images[0].id == 2311);
-    assert(images[1].id == 1951);
-    assert(images[2].id == 1171);
-    assert(images[3].id == 1427);
-    assert(images[4].id == 1489);
-    assert(images[5].id == 2473);
-    assert(images[6].id == 2971);
-    assert(images[7].id == 2729);
-    assert(images[8].id == 3079);
+    assert(images[2311].id == 2311);
+    assert(images[1951].id == 1951);
+    assert(images[1171].id == 1171);
+    assert(images[1427].id == 1427);
+    assert(images[1489].id == 1489);
+    assert(images[2473].id == 2473);
+    assert(images[2971].id == 2971);
+    assert(images[2729].id == 2729);
+    assert(images[3079].id == 3079);
 
-    for (const auto &image : images) {
+    for (const auto &it : images) {
+        auto &image = it.second;
         assert(image.tiles.size() == 10);
         for (size_t i = 0; i < image.tiles.size(); ++i) {
             assert(image.tiles[i].size() == 10);
