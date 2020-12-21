9f4a8a5cce2d5dc921f05beb6e299b84f8c2be34
Shohei YOSHIDA <syohex@gmail.com>
2020-12-21 17:56:28 +0900

    Implement day21 part01

--- 2020/day21/main.cpp
+++ 2020/day21/main.cpp
@@ -1,13 +1,16 @@
 #include <cassert>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <sstream>
+#include <map>
+#include <set>
 
 namespace {
 
 struct Food {
-    std::vector<std::string> ingredients;
-    std::vector<std::string> allergens;
+    std::set<std::string> ingredients;
+    std::set<std::string> allergens;
 
     bool operator==(const Food &other) const {
         return ingredients == other.ingredients && allergens == other.allergens;
@@ -36,9 +39,9 @@ std::vector<Food> ParseInput(T &input_stream) {
             }
 
             if (is_ingredient_part) {
-                food.ingredients.push_back(str);
+                food.ingredients.insert(str);
             } else {
-                food.allergens.push_back(str);
+                food.allergens.insert(str);
             }
 
             if (line[p] == ')') {
@@ -56,6 +59,78 @@ std::vector<Food> ParseInput(T &input_stream) {
     return foods;
 }
 
+int Solve01(const std::vector<Food> &foods) {
+    std::set<std::string> all_ingredients;
+    std::set<std::string> all_allergens;
+    for (const auto &food : foods) {
+        for (const auto &ingredient : food.ingredients) {
+            all_ingredients.insert(ingredient);
+        }
+        for (const auto &allergen : food.allergens) {
+            all_allergens.insert(allergen);
+        }
+    }
+
+    std::map<std::string, std::set<std::string>> table;
+    for (const auto &ingredient : all_ingredients) {
+        table[ingredient] = all_allergens;
+    }
+
+    while (true) {
+        for (auto &it : table) {
+            auto &ingredient = it.first;
+
+            for (const auto &food : foods) {
+                if (food.ingredients.find(ingredient) == food.ingredients.end()) {
+                    for (const auto &allergen : food.allergens) {
+                        it.second.erase(allergen);
+                    }
+                }
+            }
+
+            if (it.second.size() == 1) {
+                auto decided = (*it.second.begin());
+                for (auto &it2 : table) {
+                    if (it.first == it2.first) {
+                        continue;
+                    }
+                    it2.second.erase(decided);
+                }
+            }
+        }
+
+        bool ok = true;
+        for (const auto &it : table) {
+            if (it.second.size() >= 2) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) {
+            break;
+        }
+    }
+
+    std::set<std::string> safes;
+    for (const auto &it : table) {
+        if (it.second.empty()) {
+            safes.insert(it.first);
+        }
+    }
+
+    int ret = 0;
+    for (const auto &safe : safes) {
+        for (const auto &food : foods) {
+            if (food.ingredients.find(safe) != food.ingredients.end()) {
+                ++ret;
+            }
+        }
+    }
+
+    return ret;
+}
+
 void Test01() {
     std::string input(R"(mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
@@ -66,30 +141,35 @@ sqjhc mxmxvkd sbzzf (contains fish))");
     auto foods = ParseInput(ss);
     assert(foods.size() == 4);
     {
-        std::vector<std::string> ingredients{"mxmxvkd", "kfcds", "sqjhc", "nhms"};
-        std::vector<std::string> allergens{"dairy", "fish"};
+        std::set<std::string> ingredients{"mxmxvkd", "kfcds", "sqjhc", "nhms"};
+        std::set<std::string> allergens{"dairy", "fish"};
         assert((foods[0] == Food{ingredients, allergens}));
     }
     {
-        std::vector<std::string> ingredients{"trh", "fvjkl", "sbzzf", "mxmxvkd"};
-        std::vector<std::string> allergens{"dairy"};
+        std::set<std::string> ingredients{"trh", "fvjkl", "sbzzf", "mxmxvkd"};
+        std::set<std::string> allergens{"dairy"};
         assert((foods[1] == Food{ingredients, allergens}));
     }
     {
-        std::vector<std::string> ingredients{"sqjhc", "fvjkl"};
-        std::vector<std::string> allergens{"soy"};
+        std::set<std::string> ingredients{"sqjhc", "fvjkl"};
+        std::set<std::string> allergens{"soy"};
         assert((foods[2] == Food{ingredients, allergens}));
     }
     {
-        std::vector<std::string> ingredients{"sqjhc", "mxmxvkd", "sbzzf"};
-        std::vector<std::string> allergens{"fish"};
+        std::set<std::string> ingredients{"sqjhc", "mxmxvkd", "sbzzf"};
+        std::set<std::string> allergens{"fish"};
         assert((foods[3] == Food{ingredients, allergens}));
     }
+
+    assert(Solve01(foods) == 5);
 }
 
 } // namespace
 
 int main() {
     Test01();
+
+    auto foods = ParseInput(std::cin);
+    std::cout << "Part01:" << Solve01(foods) << std::endl;
     return 0;
 }
