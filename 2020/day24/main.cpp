b645914bd2facee1a112b9a8539223f0b0bc301a
Shohei YOSHIDA <syohex@gmail.com>
2020-12-24 15:04:51 +0900

    Add day24 part01

--- 2020/day24/main.cpp
+++ 2020/day24/main.cpp
@@ -52,6 +52,56 @@ std::vector<std::vector<Direction>> ParseInput(T &stream) {
     return ret;
 }
 
+int Solve01(const std::vector<std::vector<Direction>> &instructions) {
+    std::map<std::vector<int>, bool> m;
+    for (const auto &dir : instructions) {
+        int x = 0;
+        int y = 0;
+        for (auto d : dir) {
+            switch (d) {
+            case Direction::kEast:
+                x += 2;
+                break;
+            case Direction::kSouthEast:
+                x += 1;
+                y -= 1;
+                break;
+            case Direction::kSouthWest:
+                x -= 1;
+                y -= 1;
+                break;
+            case Direction::kWest:
+                x -= 2;
+                break;
+            case Direction::kNorthWest:
+                x -= 1;
+                y += 1;
+                break;
+            case Direction::kNorthEast:
+                x += 1;
+                y += 1;
+                break;
+            }
+        }
+
+        std::vector<int> key{x, y};
+        if (m.find(key) == m.end()) {
+            m[key] = true; // black
+        } else {
+            m[key] = !m[key]; // flip
+        }
+    }
+
+    int ret = 0;
+    for (const auto &it : m) {
+        if (it.second) {
+            ++ret;
+        }
+    }
+
+    return ret;
+}
+
 void Test01() {
     std::string input(R"(sesenwnenenewseeswwswswwnenewsewsw
 neeenesenwnwwswnenewnwwsewnenwseswesw
@@ -75,13 +125,19 @@ neswnwewnwnwseenwseesewsenwsweewe
 wseweeenwnesenwwwswnew)");
 
     std::istringstream ss(input);
-    auto directions = ParseInput(ss);
-    assert(directions.size() == 20);
+    auto instructions = ParseInput(ss);
+    assert(instructions.size() == 20);
+    assert(instructions[0].size() == 20);
+
+    assert(Solve01(instructions) == 10);
 }
 
 } // namespace
 
 int main() {
     Test01();
+
+    auto instructions = ParseInput(std::cin);
+    std::cout << "Part01: " << Solve01(instructions) << std::endl;
     return 0;
 }
