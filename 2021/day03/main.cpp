2e218a8b2653295d3641146ae87fc644aaef5c2e
Shohei YOSHIDA <syohex@gmail.com>
2021-12-03 15:11:45 +0900

    Add Day03 part2

--- 2021/day03/main.cpp
+++ 2021/day03/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+#include <set>
 
 namespace {
 
@@ -30,6 +31,84 @@ std::int64_t PowerConsumption(const std::vector<std::string> &values) {
     return gamma * epsilon;
 }
 
+std::int64_t BinaryToNumber(const std::string &s) {
+    std::int64_t ret = 0;
+    for (char c : s) {
+        ret = ret * 2 + (c - '0');
+    }
+
+    return ret;
+}
+
+std::int64_t LifeSupportRating(const std::vector<std::string> &values) {
+    int len = values[0].size();
+    std::set<std::string> oxygen_cand(values.begin(), values.end());
+    std::set<std::string> co2_cand(values.begin(), values.end());
+
+    const auto is_more_ones = [](const std::set<std::string> &s, size_t index) -> bool {
+        size_t ones = 0, zeros = 0;
+        for (const auto &it : s) {
+            if (it[index] == '1') {
+                ++ones;
+            } else {
+                ++zeros;
+            }
+        }
+
+        return ones >= zeros;
+    };
+
+    for (size_t i = 0; i < len; ++i) {
+        if (oxygen_cand.size() >= 2) {
+            if (is_more_ones(oxygen_cand, i)) {
+                for (auto it = oxygen_cand.begin(); it != oxygen_cand.end();) {
+                    const auto &value = *it;
+                    if (value[i] == '0') {
+                        it = oxygen_cand.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            } else {
+                for (auto it = oxygen_cand.begin(); it != oxygen_cand.end();) {
+                    const auto &value = *it;
+                    if (value[i] == '1') {
+                        it = oxygen_cand.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            }
+        }
+        if (co2_cand.size() >= 2) {
+            if (is_more_ones(co2_cand, i)) {
+                for (auto it = co2_cand.begin(); it != co2_cand.end();) {
+                    const auto &value = *it;
+                    if (value[i] == '1') {
+                        it = co2_cand.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            } else {
+                for (auto it = co2_cand.begin(); it != co2_cand.end();) {
+                    const auto &value = *it;
+                    if (value[i] == '0') {
+                        it = co2_cand.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            }
+        }
+    }
+
+    auto oxygen = *oxygen_cand.begin();
+    auto co2 = *co2_cand.begin();
+    auto ret = BinaryToNumber(oxygen) * BinaryToNumber(co2);
+    return ret;
+}
+
 void Test1() {
     // clang-format off
     std::vector<std::string> values {
@@ -51,10 +130,34 @@ void Test1() {
     assert(PowerConsumption(values) == 198);
 }
 
+void Test2() {
+    // clang-format off
+    std::vector<std::string> values {
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    };
+    // clang-format on
+
+    assert(BinaryToNumber("10111") == 23);
+    assert(BinaryToNumber("01010") == 10);
+    assert(LifeSupportRating(values) == 230);
+}
+
 } // namespace
 
 int main() {
     Test1();
+    Test2();
 
     std::vector<std::string> values;
     std::string tmp;
@@ -63,6 +166,8 @@ int main() {
     }
 
     auto part1 = PowerConsumption(values);
+    auto part2 = LifeSupportRating(values);
     std::cout << "Part1: " << part1 << std::endl;
+    std::cout << "Part2: " << part2 << std::endl;
     return 0;
 }
