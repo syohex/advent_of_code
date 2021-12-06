a396e80fd88e377d4bb343525080ae6336e4020e
Shohei YOSHIDA <syohex@gmail.com>
2021-12-06 14:28:36 +0900

    Add Day06 part2

--- 2021/day06/main.cpp
+++ 2021/day06/main.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdint>
+#include <map>
 
 namespace {
 
-int Part1(const std::vector<int> &data, int days) {
+size_t Part1(const std::vector<int> &data, int days) {
     std::vector<int> values(data);
 
     for (int i = 0; i < days; ++i) {
@@ -24,13 +26,40 @@ int Part1(const std::vector<int> &data, int days) {
         }
     }
 
-    return static_cast<int>(values.size());
+    return values.size();
+}
+
+size_t Part2(const std::vector<int> &data, int days) {
+    std::map<int, std::int64_t> m;
+    for (int d : data) {
+        ++m[d];
+    }
+
+    for (int i = 0; i < days; ++i) {
+        std::map<int, std::int64_t> tmp;
+        tmp[8] = m[0];
+        for (int i = 1; i <= 8; ++i) {
+            tmp[i - 1] = m[i];
+        }
+        tmp[6] += m[0];
+        m = tmp;
+    }
+
+    std::int64_t ret = 0;
+    for (const auto &it : m) {
+        ret += it.second;
+    }
+
+    return ret;
 }
 
 void Test() {
     std::vector<int> data{3, 4, 3, 1, 2};
     assert(Part1(data, 18) == 26);
     assert(Part1(data, 80) == 5934);
+    assert(Part2(data, 18) == 26);
+    assert(Part2(data, 80) == 5934);
+    assert(Part2(data, 256) == 26984457539ULL);
 }
 
 } // namespace
@@ -49,7 +78,9 @@ int main() {
     }
 
     auto part1 = Part1(data, 80);
+    auto part2 = Part2(data, 256);
 
     std::cout << "Part1: " << part1 << std::endl;
+    std::cout << "Part2: " << part2 << std::endl;
     return 0;
 }
