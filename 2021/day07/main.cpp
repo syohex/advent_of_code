6e8cf91b31c3a2f03e391468e245562c4eabe269
Shohei YOSHIDA <syohex@gmail.com>
2021-12-07 14:26:25 +0900

    Add Day07 Part2

--- 2021/day07/main.cpp
+++ 2021/day07/main.cpp
@@ -20,11 +20,40 @@ std::int64_t Part1(const std::vector<int> &data) {
         max = std::max(max, d);
     }
 
-    int ret = INT_MAX;
+    std::int64_t ret = INT_MAX;
     for (int i = min; i <= max; ++i) {
-        int sum = 0;
-        for (const auto& it : m) {
+        std::int64_t sum = 0;
+        for (const auto &it : m) {
             sum += std::abs(i - it.first) * it.second;
+        }
+
+        ret = std::min(ret, sum);
+    }
+
+    return ret;
+}
+
+std::int64_t Part2(const std::vector<int> &data) {
+    std::map<int, int> m;
+    int min = INT_MAX;
+    int max = INT_MIN;
+    for (int d : data) {
+        ++m[d];
+        min = std::min(min, d);
+        max = std::max(max, d);
+    }
+
+    std::int64_t ret = INT_MAX;
+    for (int i = min; i <= max; ++i) {
+        std::int64_t sum = 0;
+        for (const auto &it : m) {
+            std::int64_t diff = std::abs(i - it.first);
+            if (diff == 0) {
+                continue;
+            }
+
+            std::int64_t fuel = (1 + diff) * diff / 2;
+            sum += fuel * it.second;
         }
 
         ret = std::min(ret, sum);
@@ -36,6 +65,7 @@ std::int64_t Part1(const std::vector<int> &data) {
 void Test() {
     std::vector<int> data{16, 1, 2, 0, 4, 2, 7, 1, 2, 14};
     assert(Part1(data) == 37);
+    assert(Part2(data) == 168);
 }
 
 } // namespace
@@ -54,8 +84,10 @@ int main() {
     }
 
     auto part1 = Part1(data);
+    auto part2 = Part2(data);
 
     std::cout << "Part1: " << part1 << std::endl;
+    std::cout << "Part2: " << part2 << std::endl;
 
     return 0;
 }
