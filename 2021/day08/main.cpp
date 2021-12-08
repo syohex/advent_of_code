fc83f501f5d293c445a8a15bbe4148c273543767
Shohei YOSHIDA <syohex@gmail.com>
2021-12-08 16:56:39 +0900

    Add Day08 part2

--- 2021/day08/main.cpp
+++ 2021/day08/main.cpp
@@ -3,12 +3,165 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <map>
+#include <cstdint>
+#include <algorithm>
 
 namespace {
 
 struct Data {
-    std::vector<std::string> first;
-    std::vector<std::string> second;
+    std::vector<std::string> candidates;
+    std::vector<std::string> output;
+
+    std::int64_t Decode() const {
+        std::vector<std::string> numbers(10, "");
+        std::vector<bool> checked(10, false);
+        size_t one_index = FindOne();
+        size_t four_index = FindFour();
+        size_t seven_index = FindSeven();
+        size_t eight_index = FindEight();
+
+        numbers[1] = candidates[one_index];
+        numbers[4] = candidates[four_index];
+        numbers[7] = candidates[seven_index];
+        numbers[8] = candidates[eight_index];
+
+        checked[one_index] = true;
+        checked[four_index] = true;
+        checked[seven_index] = true;
+        checked[eight_index] = true;
+
+        const auto just_match = [](const std::string &target, const std::string &source) -> bool {
+            for (char c : source) {
+                if (target.find(c) == std::string::npos) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        // find 9
+        for (size_t i = 0; i < candidates.size(); ++i) {
+            if (checked[i]) {
+                continue;
+            }
+
+            if (candidates[i].size() == 6) {
+                if (just_match(candidates[i], numbers[4])) {
+                    checked[i] = true;
+                    numbers[9] = candidates[i];
+                    break;
+                }
+            }
+        }
+
+        std::vector<size_t> five_segments;
+        for (size_t i = 0; i < candidates.size(); ++i) {
+            if (checked[i]) {
+                continue;
+            }
+
+            if (candidates[i].size() == 5) {
+                five_segments.push_back(i);
+            }
+        }
+
+        for (size_t i : five_segments) {
+            if (just_match(candidates[i], numbers[1])) {
+                checked[i] = true;
+                numbers[3] = candidates[i];
+            }
+        }
+
+        for (size_t i : five_segments) {
+            if (checked[i]) {
+                continue;
+            }
+            if (just_match(numbers[9], candidates[i])) {
+                checked[i] = true;
+                numbers[5] = candidates[i];
+                break;
+            }
+        }
+
+        for (size_t i : five_segments) {
+            if (checked[i]) {
+                continue;
+            }
+
+            checked[i] = true;
+            numbers[2] = candidates[i];
+        }
+
+        for (size_t i = 0; i < candidates.size(); ++i) {
+            if (checked[i]) {
+                continue;
+            }
+
+            if (candidates[i].size() == 6) {
+                if (just_match(candidates[i], numbers[1]) && just_match(numbers[8], candidates[i])) {
+                    checked[i] = true;
+                    numbers[0] = candidates[i];
+                    break;
+                }
+            }
+        }
+
+        for (size_t i = 0; i < candidates.size(); ++i) {
+            if (checked[i]) {
+                continue;
+            }
+
+            if (candidates[i].size() == 6) {
+                checked[i] = true;
+                numbers[6] = candidates[i];
+                break;
+            }
+        }
+
+        std::map<std::string, int> m;
+        for (size_t i = 0; i < numbers.size(); ++i) {
+            std::string tmp(numbers[i]);
+            std::sort(tmp.begin(), tmp.end());
+            m[tmp] = static_cast<int>(i);
+        }
+
+        std::int64_t ret = 0;
+        for (const auto &str : output) {
+            std::string tmp(str);
+            std::sort(tmp.begin(), tmp.end());
+            if (m.find(tmp) == m.end()) {
+                throw "never reach here";
+            }
+            ret = ret * 10 + m[tmp];
+        }
+
+        return ret;
+    }
+
+    size_t FindOne() const {
+        return FindByLength(2);
+    }
+    size_t FindFour() const {
+        return FindByLength(4);
+    }
+    size_t FindSeven() const {
+        return FindByLength(3);
+    }
+    size_t FindEight() const {
+        return FindByLength(7);
+    }
+
+    size_t FindByLength(size_t len) const {
+        for (size_t i = 0; i < candidates.size(); ++i) {
+            if (candidates[i].size() == len) {
+                return i;
+            }
+        }
+
+        throw "never reach here";
+    }
 };
 
 std::vector<std::string> SplitString(const std::string &str) {
@@ -43,8 +196,8 @@ std::vector<Data> ParseInput(std::istream &ss) {
         std::getline(ss_line, tmp1, '|');
         std::getline(ss_line, tmp2, '|');
 
-        d.first = SplitString(tmp1);
-        d.second = SplitString(tmp2);
+        d.candidates = SplitString(tmp1);
+        d.output = SplitString(tmp2);
 
         ret.push_back(d);
     }
@@ -55,12 +208,21 @@ std::vector<Data> ParseInput(std::istream &ss) {
 int Part1(const std::vector<Data> &data) {
     int ret = 0;
     for (const auto &d : data) {
-        for (const auto &str : d.second) {
+        for (const auto &str : d.output) {
             size_t size = str.size();
             if (size == 2 || size == 4 || size == 3 || size == 7) {
                 ++ret;
             }
         }
+    }
+
+    return ret;
+}
+
+std::int64_t Part2(const std::vector<Data> &data) {
+    std::int64_t ret = 0;
+    for (const auto &d : data) {
+        ret += d.Decode();
     }
 
     return ret;
@@ -82,6 +244,9 @@ gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     auto data = ParseInput(ss);
     auto part1 = Part1(data);
     assert(part1 == 26);
+
+    auto part2 = Part2(data);
+    assert(part2 == 61229);
 }
 
 } // namespace
@@ -91,7 +256,9 @@ int main() {
 
     auto data = ParseInput(std::cin);
     auto part1 = Part1(data);
+    auto part2 = Part2(data);
 
     std::cout << "Part1: " << part1 << std::endl;
+    std::cout << "Part2: " << part2 << std::endl;
     return 0;
 }
