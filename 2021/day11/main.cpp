02da00ba7b478a705a8332be8f312554715151ca
Shohei YOSHIDA <syohex@gmail.com>
2021-12-11 14:48:44 +0900

    Add Day11 Part2

--- 2021/day11/main.cpp
+++ 2021/day11/main.cpp
@@ -77,6 +77,73 @@ int Part1(const std::vector<std::vector<int>> &input, int steps) {
     return ret;
 }
 
+int Part2(const std::vector<std::vector<int>> &input) {
+    auto data = input;
+    int rows = input.size();
+    int cols = input[0].size();
+
+    std::vector<std::vector<int>> moves{
+        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1},
+    };
+    std::function<void(int r, int c, std::vector<std::vector<bool>> &flashed)> f;
+    f = [&](int r, int c, std::vector<std::vector<bool>> &flashed) {
+        flashed[r][c] = true;
+
+        for (const auto &move : moves) {
+            int new_r = r + move[0];
+            int new_c = c + move[1];
+            if (new_r >= 0 && new_r < rows && new_c >= 0 && new_c < cols) {
+                ++data[new_r][new_c];
+                if (!flashed[new_r][new_c] && data[new_r][new_c] >= 10) {
+                    f(new_r, new_c, flashed);
+                }
+            }
+        }
+    };
+
+    int ret = 1;
+    int all = rows * cols;
+    while (true) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                ++data[i][j];
+            }
+        }
+
+        std::vector<std::vector<bool>> flashed(rows, std::vector<bool>(cols, false));
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] < 10) {
+                    continue;
+                }
+
+                if (!flashed[i][j]) {
+                    f(i, j, flashed);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (data[i][j] >= 10) {
+                    data[i][j] = 0;
+                    ++count;
+                }
+            }
+        }
+
+        if (count == all) {
+            return ret;
+        }
+
+        ++ret;
+    }
+
+    // never reach here
+    return -1;
+}
+
 void TestSimple() {
     std::vector<std::string> input{
         "11111", "19991", "19191", "19991", "11111",
@@ -96,6 +163,8 @@ void Test() {
     auto data = ParseInput(input);
     assert(Part1(data, 10) == 204);
     assert(Part1(data, 100) == 1656);
+
+    assert(Part2(data) == 195);
 }
 
 } // namespace
@@ -115,8 +184,10 @@ int main() {
 
     auto data = ParseInput(input);
     auto part1 = Part1(data, 100);
+    auto part2 = Part2(data);
 
     std::cout << "Part1: " << part1 << std::endl;
+    std::cout << "Part2: " << part2 << std::endl;
 
     return 0;
 }
