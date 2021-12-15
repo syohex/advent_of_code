8e137a1bfc8e6ad2caefc3d8a91a5fa62139533d
Shohei YOSHIDA <syohex@gmail.com>
2021-12-15 17:33:37 +0900

    Improve part2 by using priority queue
    45.378 seconds -> 0.293 seconds

--- 2021/day15/main.cpp
+++ 2021/day15/main.cpp
@@ -4,10 +4,11 @@
 #include <string>
 #include <sstream>
 #include <cstdint>
-#include <algorithm>
 #include <limits>
 #include <set>
 #include <list>
+#include <queue>
+#include <chrono>
 
 namespace {
 
@@ -130,6 +131,80 @@ std::int64_t Part2(const std::vector<std::vector<int>> &input) {
     return dp[rows - 1][cols - 1];
 }
 
+std::int64_t Part2Improved(const std::vector<std::vector<int>> &input) {
+    int base_rows = input.size();
+    int base_cols = input[0].size();
+    std::vector<std::vector<int>> data(base_rows * 5, std::vector<int>(base_cols * 5, 0));
+    int rows = data.size();
+    int cols = data[0].size();
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            int base_row = base_rows * i;
+            for (int m = 0; m < base_rows; ++m) {
+                int base_col = base_cols * j;
+                for (int n = 0; n < base_cols; ++n) {
+                    int row = base_row + m;
+                    int col = base_col + n;
+
+                    if (i == 0 && j == 0) {
+                        data[row][col] = input[m][n];
+                    } else {
+                        if (i == 0) {
+                            int value = data[row][col - base_cols] + 1;
+                            data[row][col] = value == 10 ? 1 : value;
+                        } else {
+                            int value = data[row - base_rows][col] + 1;
+                            data[row][col] = value == 10 ? 1 : value;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    struct PointData {
+        int risk;
+        int x;
+        int y;
+    };
+
+    const auto cmp = [](const PointData &a, const PointData &b) { return a.risk > b.risk; };
+
+    std::vector<std::vector<int>> steps{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    std::priority_queue<PointData, std::vector<PointData>, decltype(cmp)> q(cmp);
+    q.push({0, 0, 0});
+
+    std::set<std::pair<int, int>> checked;
+    checked.insert({0, 0});
+
+    while (!q.empty()) {
+        auto p = q.top();
+        q.pop();
+
+        if (p.x == rows - 1 && p.y == cols - 1) {
+            return p.risk;
+        }
+
+        for (const auto &step : steps) {
+            int row = p.x + step[0];
+            int col = p.y + step[1];
+            if (checked.find({row, col}) != checked.end()) {
+                continue;
+            }
+
+            if (row >= 0 && row < rows && col >= 0 && col < cols) {
+                int value = p.risk + data[row][col];
+                q.push({value, row, col});
+                checked.insert({row, col});
+            }
+        }
+    }
+
+    assert(false && "never reach here");
+    return -1;
+}
+
 void Test() {
     std::string input(R"(1163751742
 1381373672
@@ -157,7 +232,18 @@ int main() {
 
     auto data = ParseInput(std::cin);
     auto part1 = Part1(data);
+
+    auto start = std::chrono::system_clock::now();
     auto part2 = Part2(data);
+    auto end = std::chrono::system_clock::now();
+
+    std::cout << "Original: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
+
+    start = std::chrono::system_clock::now();
+    part2 = Part2Improved(data);
+    end = std::chrono::system_clock::now();
+
+    std::cout << "Improved: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
 
     std::cout << "Part1: " << part1 << std::endl;
     std::cout << "Part2: " << part2 << std::endl;
