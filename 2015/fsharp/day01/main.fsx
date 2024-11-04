f93a4dbbdb6c19dcaa0061329db0dac39a6932de
Shohei YOSHIDA <syohex@gmail.com>
2024-11-04 21:14:10 +0900

    Add 2015 day01 in F#

---
+++ 2015/fsharp/day01/main.fsx
@@ -0,0 +1,44 @@
+open System.IO
+
+let problem1 (s: string) : int =
+    let rec f cs acc =
+        match cs with
+        | [] -> acc
+        | '(' :: t -> f t (acc + 1)
+        | ')' :: t -> f t (acc - 1)
+        | h :: _ -> failwithf "invalid input %c" h
+
+    f (Seq.toList s) 0
+
+let problem2 (s: string) : int =
+    let rec f cs steps acc =
+        if acc = -1 then
+            steps
+        else
+            match cs with
+            | [] -> failwith "never reach here"
+            | '(' :: t -> f t (steps + 1) (acc + 1)
+            | ')' :: t -> f t (steps + 1) (acc - 1)
+            | h :: _ -> failwithf "invalid input %c" h
+
+    f (Seq.toList s) 0 0
+
+let testInput =
+    [ "(())"; "()()"; "((("; "(()(()("; "))((((("; "())"; "))("; ")))"; ")())())" ]
+
+// [0, 0, 3, 3, 3, -1, -1, -3, -3]
+testInput |> List.map problem1
+
+let input = File.ReadLines("../../input/day01.txt") |> Seq.head
+
+// 232
+problem1 input
+
+// 1
+problem2 ")"
+
+// 5
+problem2 "()())"
+
+// 1783
+problem2 input
