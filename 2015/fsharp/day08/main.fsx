e77c3a7e3bfd97c63adc99d0ab9c9f6f80faf088
Shohei YOSHIDA <syohex@gmail.com>
2024-11-04 18:52:54 +0900

    Remove needless calculation

--- 2015/fsharp/day08/main.fsx
+++ 2015/fsharp/day08/main.fsx
@@ -1,11 +1,11 @@
 open System
 open System.IO
 
-let parseLine (s: string) : int * int =
+let parseLine (s: string) : int =
     let rec f cs acc =
         match cs with
         | [] -> failwith "never reach here"
-        | '"' :: [] -> s.Length, acc
+        | '"' :: [] -> s.Length - acc
         | '\\' :: '\\' :: t
         | '\\' :: '"' :: t -> f t (acc + 1)
         | '\\' :: 'x' :: c1 :: c2 :: t when Char.IsAsciiHexDigit(c1) && Char.IsAsciiHexDigit(c2) -> f t (acc + 1)
@@ -14,10 +14,7 @@ let parseLine (s: string) : int * int =
     f (s |> Seq.toList |> List.tail) 0
 
 let problem1 (lines: string list) : int =
-    lines
-    |> List.map parseLine
-    |> List.fold (fun (acc1, acc2) (total, memory) -> acc1 + total, acc2 + memory) (0, 0)
-    |> fun (total, memory) -> total - memory
+    lines |> List.map parseLine |> List.reduce (+)
 
 let testInput = [ "\"\""; "\"abc\""; "\"aaa\\\"aaa\""; "\"\\x27\"" ]
 // 23, 11
@@ -26,4 +23,5 @@ problem1 testInput
 let input = File.ReadLines("../../input/day08.txt") |> Seq.toList
 
 let ret1 = problem1 input
+// 1371
 printfn "ret1=%d" ret1
