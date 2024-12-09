b90a0262e4ed2e9b4d8cb852e6dcd08bcc0894c3
Shohei YOSHIDA <syohex@gmail.com>
2024-12-09 16:05:37 +0900

    Add 2015 day03 part2 in C++

--- 2015/cpp/day03.cpp
+++ 2015/cpp/day03.cpp
@@ -36,10 +36,47 @@ auto Problem1(const std::string &input) -> int {
     return s.size();
 }
 
+auto Problem2(const std::string &input) -> int {
+    set<pair<int, int>> s;
+    s.insert({0, 0});
+
+    int rows[2] = {0, 0};
+    int cols[2] = {0, 0};
+    int index = 0;
+    for (char c : input) {
+        switch (c) {
+        case '^':
+            rows[index] -= 1;
+            break;
+        case '>':
+            cols[index] += 1;
+            break;
+        case 'v':
+            rows[index] += 1;
+            break;
+        case '<':
+            cols[index] -= 1;
+            break;
+        default:
+            assert(!"never reach here");
+            break;
+        }
+
+        s.insert({rows[index], cols[index]});
+        index = index == 0 ? 1 : 0;
+    }
+
+    return s.size();
+}
+
 void Test() {
     assert(Problem1(">") == 2);
     assert(Problem1("^>v<") == 4);
-    assert(Problem1("^v^v^v^v") == 2);
+    assert(Problem1("^v^v^v^v^v") == 2);
+
+    assert(Problem2("^v") == 3);
+    assert(Problem2("^>v<") == 3);
+    assert(Problem2("^v^v^v^v^v") == 11);
 }
 
 int main() {
@@ -52,6 +89,10 @@ int main() {
     int ret1 = Problem1(input);
     assert(ret1 == 2572);
 
+    int ret2 = Problem2(input);
+    assert(ret2 == 2631);
+
     printf("ret1 = %d\n", ret1);
+    printf("ret2 = %d\n", ret2);
     return 0;
 }
