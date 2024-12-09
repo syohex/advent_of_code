de09b6a54ce07630863c6c8082e7b04755ce7f7b
Shohei YOSHIDA <syohex@gmail.com>
2024-12-09 19:47:00 +0900

    Add day05 part2 in C++

--- 2015/cpp/day05.cpp
+++ 2015/cpp/day05.cpp
@@ -54,12 +54,55 @@ auto Problem1(const vector<string> &input) -> int {
     return ret;
 }
 
+auto HasTwoLettersPair(const string &s) -> bool {
+    size_t len = s.size();
+    for (size_t i = 0; i < len - 1; ++i) {
+        auto substr = s.substr(i, 2);
+        auto pos = s.find(substr, i + 1);
+        if (pos != string::npos && pos >= i + 2) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+auto HasWrapping(const string &s) -> bool {
+    for (size_t i = 2; i < s.size(); ++i) {
+        if (s[i - 2] == s[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+auto IsNiceString2(const string &s) -> bool {
+    return HasTwoLettersPair(s) && HasWrapping(s);
+}
+
+auto Problem2(const vector<string> &input) -> int {
+    int ret = 0;
+    for (const auto &s : input) {
+        if (IsNiceString2(s)) {
+            ++ret;
+        }
+    }
+
+    return ret;
+}
+
 auto Test() -> void {
     assert(IsNiceString("ugknbfddgicrmopn"));
     assert(IsNiceString("aaa"));
     assert(!IsNiceString("jchzalrnumimnmhp"));
     assert(!IsNiceString("haegwjzuvuyypxyu"));
     assert(!IsNiceString("dvszwmarrgswjxmb"));
+
+    assert(IsNiceString2("qjhvhtzxzqqjkmpb"));
+    assert(IsNiceString2("xxyxx"));
+    assert(!IsNiceString2("uurcxstgmygtbstg"));
+    assert(!IsNiceString2("ieodomkazucvgmuy"));
 }
 
 int main() {
@@ -75,6 +118,10 @@ int main() {
     int ret1 = Problem1(input);
     assert(ret1 == 258);
 
+    int ret2 = Problem2(input);
+    assert(ret2 == 53);
+
     printf("ret1 = %d\n", ret1);
+    printf("ret2 = %d\n", ret2);
     return 0;
 }
