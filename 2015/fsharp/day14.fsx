9fcf8e37fdf027a704325465454b4eab6132160b
Shohei YOSHIDA <syohex@gmail.com>
2024-12-12 11:55:45 +0900

    Add 2015 day14 part1 in F#

--- 2015/fsharp/day14.fsx
+++ 2015/fsharp/day14.fsx
@@ -1,3 +1,4 @@
+open System.IO
 open System.Text.RegularExpressions
 
 type Data =
@@ -22,8 +23,41 @@ let parse (input: string list) : Data list =
         else
             failwithf "invalid input %s" s)
 
+let afterNSeconds (seconds: int) (d: Data) : int =
+    let rec afterNSeconds' seconds isFlying (d: Data) acc =
+        if seconds <= 0 then
+            acc
+        else
+            let actionTime =
+                if isFlying then
+                    min seconds d.FlyingSeconds
+                else
+                    min seconds d.RestingSeconds
+
+            if isFlying then
+                afterNSeconds' (seconds - actionTime) false d (acc + d.Velocity * actionTime)
+            else
+                afterNSeconds' (seconds - actionTime) true d acc
+
+    afterNSeconds' seconds true d 0
+
+let problem1 (seconds: int) (input: Data list) : int =
+    input
+    |> List.map (fun d -> afterNSeconds seconds d)
+    |> List.sortDescending
+    |> List.head
+
 let testInput =
     [ "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."
       "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds." ]
 
-parse testInput
+let testData = parse testInput
+// 1120
+problem1 1000 testData
+
+let input = File.ReadAllLines("../input/day14.txt") |> Array.toList
+let data = parse input
+
+let ret1 = problem1 2503 data
+
+printfn "ret1 = %d" ret1
