749aedb3c2325645aa30e8335566c3a92ca1b913
Shohei YOSHIDA <syohex@gmail.com>
2024-12-16 00:46:53 +0900

    Add 2015 day16 part1 in F#

---
+++ 2015/fsharp/day16.fsx
@@ -0,0 +1,48 @@
+open System
+open System.IO
+
+let parse (s: string) : Map<string, int> =
+    let rec parse' parts acc =
+        match parts with
+        | [] -> acc
+        | _ :: [] -> failwithf "invalid input %s" s
+        | name :: count :: t -> parse' t (Map.add name (int count) acc)
+
+    let parts =
+        s
+            .Replace(":", "")
+            .Replace(",", "")
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+
+    parse' (List.skip 2 parts) Map.empty
+
+let isMatched (aunt: Map<string, int>) (compounds: Map<string, int>) : bool =
+    compounds
+    |> Map.forall (fun k v ->
+        match Map.tryFind k aunt with
+        | None -> true
+        | Some(num) -> num = v)
+
+let problem1 (input: Map<string, int> list) (compounds: Map<string, int>) : int =
+    input |> List.findIndex (fun m -> isMatched m compounds) |> ((+) 1)
+
+let compounds =
+    """children: 3
+    cats: 7
+    samoyeds: 2
+    pomeranians: 3
+    akitas: 0
+    vizslas: 0
+    goldfish: 5
+    trees: 3
+    cars: 2
+    perfumes: 1"""
+        .Replace('\n', ' ')
+    |> parse
+
+let input =
+    File.ReadAllLines("../input/day16.txt") |> Array.map parse |> Array.toList
+
+let ret1 = problem1 input compounds
+printfn "ret1 = %d" ret1
