01b3ecfd4984d7a105e4011a62cab84f21804509
Shohei YOSHIDA <syohex@gmail.com>
2024-12-18 13:42:22 +0900

    Add 2015 day17 part2 in F#

--- 2015/fsharp/day17.fsx
+++ 2015/fsharp/day17.fsx
@@ -16,11 +16,36 @@ let problem1 (nums: int list) (limit: int) : int =
 
     f nums limit 0
 
+let problem2 (nums: int list) (limit: int) : int =
+    let rec f nums limit sum len acc =
+        if sum = limit then
+            match Map.tryFind len acc with
+            | Some(v) -> Map.add len (v + 1) acc
+            | None -> Map.add len 1 acc
+        elif sum > limit then
+            acc
+        else
+            match nums with
+            | [] -> acc
+            | h :: t ->
+                let acc = f t limit (sum + h) (len + 1) acc
+                f t limit sum len acc
+
+    let m = f nums limit 0 0 Map.empty
+    let minKey = m |> Map.keys |> Seq.min
+    Map.find minKey m
+
+let testData = [ 20; 15; 10; 5; 5 ]
 // 4
-problem1 [20;15;10;5;5] 25
+problem1 testData 25
+// 3
+problem2 testData 25
 
 let input = File.ReadLines("../input/day17.txt") |> Seq.map int |> Seq.toList
 
 let ret1 = problem1 input 150
 // 1638
 printfn "ret1 = %d" ret1
+
+let ret2 = problem2 input 150
+printfn "ret2 = %d" ret2
