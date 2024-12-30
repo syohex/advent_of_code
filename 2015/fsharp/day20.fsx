611122f6bb46ed37919322693175472a47b978aa
Shohei YOSHIDA <syohex@gmail.com>
2024-12-30 15:57:04 +0900

    Add 2015 day20 part2 in F#

--- 2015/fsharp/day20.fsx
+++ 2015/fsharp/day20.fsx
@@ -21,6 +21,33 @@ let problem1 (input: int) : int =
 
     f 1
 
+let problem2 (input: int) : int =
+    let lowest = Math.Ceiling(double input / 11.0) |> int
+
+    let rec f i (dp: int[]) =
+        seq { 1..50 }
+        |> Seq.iter (fun j ->
+            let k = i * j
+
+            if k < lowest then
+                dp.[k] <- dp.[k] + i * 11)
+
+        if (i + 1) >= lowest then
+            match Array.tryFindIndex (fun n -> n >= input) dp with
+            | None -> failwith "never reach here"
+            | Some(v) -> v
+        else
+            f (i + 1) dp
+
+    let dp = Array.zeroCreate lowest
+    f 1 dp
+
 problem1 150
+
 let ret1 = problem1 29000000
+// 665280
 printfn "ret1 = %d" ret1
+
+let ret2 = problem2 29000000
+// 705600
+printfn "ret2 = %d" ret2
