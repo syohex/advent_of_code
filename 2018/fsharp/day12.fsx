ded2ee93906e4047c356003dc89894ee8ae2125f
Shohei YOSHIDA <syohex@gmail.com>
2026-02-01 16:40:27 +0900

    accept problem1

--- 2018/fsharp/day12.fsx
+++ 2018/fsharp/day12.fsx
@@ -24,13 +24,13 @@ let parseInput (input: string list) : string * Map<int, char> =
     else
         failwithf "cannot parse state line '%s'" (List.head input)
 
-let adjustState (state: (int * char) list) : (int * char) list * int =
+let adjustState (state: char list) : char list * int =
     let f state =
-        let pos = List.findIndex (fun (_, v) -> v = '#') state
+        let pos = List.findIndex ((=) '#') state
 
         if pos < 4 then
             let diff = 4 - pos
-            seq { 1..diff } |> Seq.fold (fun acc _ -> (-1, '.') :: acc) state, diff
+            seq { 1..diff } |> Seq.fold (fun acc _ -> '.' :: acc) state, diff
         else
             state, 0
 
@@ -38,32 +38,29 @@ let adjustState (state: (int * char) list) : (int * char) list * int =
     let state', _ = f (List.rev state)
     List.rev state', diff
 
-let problem1 (state: string) (table: Map<int, char>) (count: int) : int =
-    let rec f i (state: (int * char) list) basePos =
+let problem1 (state: string) (table: Map<int, char>) (count: int64) : int =
+    let rec f i (state: char list) basePos =
         if i >= count then
-            let zeroPos = List.tryFindIndex (fun (i, _) -> i = 0) state |> Option.defaultValue 0
-
             state
             |> List.indexed
-            |> List.fold (fun acc (i, (_, v)) -> if v = '#' then acc + i - zeroPos else acc) 0
+            |> List.fold (fun acc (i, v) -> if v = '#' then acc + i + basePos else acc) 0
         else
             let state, diff = adjustState state
+            let basePos = basePos - diff
 
             let nextState =
                 state
                 |> List.windowed 5
-                |> List.fold
-                    (fun acc v ->
-                        let p = v |> List.map snd |> patternToInt
-                        let c = Map.tryFind p table |> Option.defaultValue '.'
-                        let hasOrigin = v |> List.item 2 |> fst |> ((=) 0)
-                        if hasOrigin then (0, c) :: acc else (-1, c) :: acc)
-                    []
-                |> List.rev
+                |> List.map patternToInt
+                |> List.map (fun p -> Map.tryFind p table |> Option.defaultValue '.')
 
-            f (i + 1) nextState (basePos + diff)
+            let firstHash = List.findIndex ((=) '#') nextState
+            let basePos = 2 + basePos + firstHash
+            let nextState = List.skipWhile ((=) '.') nextState
 
-    f 0 (Seq.toList state |> List.indexed) 0
+            f (i + 1L) nextState basePos
+
+    f 0 (Seq.toList state) 0
 
 let testInput =
     "initial state: #..#.#..##......###...###
@@ -88,7 +85,10 @@ let testInput =
 let testState, testTable = parseInput testInput
 problem1 testState testTable 20
 
-let state, table = "../input/day12.txt" |> File.ReadAllLines |> Seq.toList |> parseInput
-// not 3189
-let ret1 = problem1 state table 20
+let state, table =
+    "../input/day12.txt" |> File.ReadAllLines |> Seq.toList |> parseInput
+let ret1 = problem1 state table 20L
+let ret2 = problem1 state table 50000000000L
+
 printfn "problem1 = %d" ret1
+printfn "problem2 = %d" ret2
